edb80a7eb79e0560f4a31e05c6b3f195b27791bb
Alex D. Kim <alterprist@gmail.com>
2018-02-17 01:23:14 -0500

    removed port, working on local branch

--- src/client/app/streampage.jsx
+++ src/client/app/streampage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import bms_list from './insane_bms.json';
 import socketIOClient from 'socket.io-client';
-const socket = socketIOClient('http://localhost:80')
+const socket = socketIOClient('http://localhost')
 
 class StreamPage extends React.Component {
   constructor(props){
