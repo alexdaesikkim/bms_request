9348295ebd6f1db76c5a7906a03365f973c5dfec
Alex D. Kim <alterprist@gmail.com>
2018-02-18 01:21:47 -0500

    table updated, search implemented

--- src/client/app/userpage.jsx
+++ src/client/app/userpage.jsx
@@ -14,11 +14,40 @@ class UserPage extends React.Component {
     })
     this.state = {
       songs: songs,
-      server_address: "http://localhost:80",
+      search_term: '',
+      level_field: '',
+      filtered_songs: songs,
       message: false,
       requests: []
     }
     this.removeAllSongs = this.removeAllSongs.bind(this);
+    this.titleFilter = this.titleFilter.bind(this);
+    this.levelFilter = this.levelFilter.bind(this);
+  }
+
+  songsFilter(search_term, level_field){
+    var filtered_songs = this.state.songs.filter(function(obj){
+      var index = obj.title.toLowerCase().indexOf(search_term);
+      var obj_level = obj.level.toString();
+      if(obj_level === "99") obj.level = "???"
+      var level_index = obj_level.indexOf(level_field.toString());
+      return index !== -1 && level_index !== -1
+    })
+    this.setState({
+      search_term: search_term,
+      level_field: level_field,
+      filtered_songs: filtered_songs
+    })
+  }
+
+  levelFilter(event){
+    var level_field = event.target.value;
+    this.songsFilter(this.state.search_term, level_field)
+  }
+
+  titleFilter(event){
+    var search_term = event.target.value;
+    this.songsFilter(search_term, this.state.level_field)
   }
 
   componentDidMount(){
@@ -52,15 +81,17 @@ class UserPage extends React.Component {
   }
 
   render () {
-    var song_rendered = this.state.songs.map(function(song){
+    var song_list = (this.state.search_term === '' && this.state.level_field === '' ? this.state.songs : this.state.filtered_songs);
+    var song_rendered = song_list.map(function(song){
       return(
         <SongList song={song} key={song["id"]} />
       )
     })
     var current_song_requests = this.state.requests.map(function(song){
+      var level = (song.level === 99 ? "???" : song.level)
       return(
-        <span>
-          {song.title + " (★" + song.level + ")"}&ensp;&ensp;
+        <span key={"request_"+song.id}>
+          {song.title + " (★" + level + ")"}&ensp;&ensp;
         </span>
       )
     })
@@ -69,17 +100,32 @@ class UserPage extends React.Component {
         <h5>Current Requests:</h5>
         {current_song_requests}
         <br/>
+        <div className="row">
+          <div className="col-8">
+            <input type="text" className="form-control" placeholder="Search for Songs" onChange={this.titleFilter}></input>
+          </div>
+          <div className="col-4">
+            <input type="text" className="form-control" placeholder="Filter by Level" onChange={this.levelFilter}></input>
+          </div>
+        </div>
+        <br/>
         <table className="table">
           <thead>
-            <tr>
-              <th scope="col">Song Name</th>
-              <th scope="col">Artist</th>
-              <th scope="col">Level</th>
-              <th scope="col"></th>
+            <tr className="d-flex">
+              <th scope="col" className="col-5">Song Name</th>
+              <th scope="col" className="col-5">Artist</th>
+              <th scope="col" className="col-1">Level</th>
+              <th scope="col" className="col-1"></th>
             </tr>
           </thead>
           <tbody>
-            {song_rendered}
+            {song_list.length > 0 ? song_rendered : (
+              <tr>
+                <td className="no_match" colspan="4">
+                  No match. Try different search terms.
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
@@ -99,12 +145,13 @@ class SongList extends React.Component{
   }
 
   render(){
+    var level = (this.props.song.level === 99 ? "???" : this.props.song.level);
     return(
-      <tr className={this.props.song.queue ? "disabled" : "enabled"}>
-        <td>{this.props.song.title}</td>
-        <td>{this.props.song.artist}</td>
-        <td>{"★" + this.props.song.level}</td>
-        <td>{this.props.song.queue ? "In Queue" : <button onClick={this.sendSongRequest}>Request</button>}</td>
+      <tr className={this.props.song.queue ? "disabled d-flex" : "enabled d-flex"}>
+        <td className="col-5">{this.props.song.title}</td>
+        <td className="col-5">{this.props.song.artist}</td>
+        <td className="col-1">{"★" + level}</td>
+        <td className="col-1">{this.props.song.queue ? "In Queue" : <button onClick={this.sendSongRequest}>Request</button>}</td>
       </tr>
     )
   }
